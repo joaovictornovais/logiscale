583993c04923bf4deab5529c8b77f7a408515bce
Joao Victor <joao.dipaay@gmail.com>
2026-02-09 19:48:13 -0300

    initial config

---
+++ cmd/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("LogiScale is running!"))
+	})
+
+	port := ":3000"
+
+	log.Println("Server starting on port " + port)
+	http.ListenAndServe(port, r)
+}
